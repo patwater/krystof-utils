89497a7db5dd2cc391fea0a371452671760b66e4
Krystof Litomisky <krystof@litomisky.com>
2015-10-11 17:15:01 -0700

    sliding window: keep track of which images we've already processed

--- python/sliding_window_training_data_generator.py
+++ python/sliding_window_training_data_generator.py
@@ -28,17 +28,37 @@ class Training_data_generator(object):
         # directory from where to read the images
         self.SOURCEDIR = 'raw_images'
 
+        # files where to keep track of what's been processed, and how
+        self.POSFILE        = 'positives.json'
+        self.NEGFILE        = 'negatives.json'
+        self.NEUTRAL_FILE   = 'neutral.json'
+        self.PROCESSED_FILE = 'processed.txt'
+
 
     def train(self):
         '''
         TODO
         '''
 
+        # get the list of images we've alredy processed
+        if not os.path.isfile(self.PROCESSED_FILE):
+            # make the file if it didn't exist
+            with open(self.PROCESSED_FILE, 'a') as fp:
+                pass
+        with open(self.PROCESSED_FILE, 'r') as fp:
+            processed = [s.strip() for s in fp.readlines()]
+
         # for each file in the source directory
         for fn in os.listdir(self.SOURCEDIR):
         
             # is it an image?
             if fn[-4:] != '.jpg':
+                continue
+
+            # have we processed this image already?
+            # if so, skip it
+            if fn in processed:
+                MSG('skipping {} (already processed)'.format(fn))
                 continue
 
             fullfn = os.path.join(self.SOURCEDIR, fn)
@@ -70,12 +90,43 @@ class Training_data_generator(object):
                 # show the image
                 cv2.imshow('img', di)
 
-                # TODO: get user input (good/bad/unsure)
-                cv2.waitKey(0)
+                # get some valid input from the user
+                key = None
+                while key not in [27, 65363, 65361, 65364]:
 
+                    key = cv2.waitKey(0)
+
+                    # escape: kill the program
+                    if key == 27:
+                        MSG('Exiting.')
+                        return
+
+                    # right arrow: 65363 -> positive region
+                    elif key == 65363:
+                        MSG('POSITIVE')
+
+                    # left  arrow: 65361 -> negative region
+                    elif key == 65361:
+                        MSG('negative')
+
+                    # down  arrow: 65364 -> neutral / unknown / N/A / skip
+                    elif key == 65364:
+                        MSG('neutral')
+
+                    # not valid input: ask again
+                    else:
+                        MSG('Valid input values:\n'
+                            'right arrow: positive region\n'
+                            'left  arrow: negative region\n'
+                            'down  arrow: not sure\n'
+                            'escape     : exit the program')
+
+                # get the next region
                 region = sw.get_next()
 
-                
+            # mark this image as processed
+            with open(self.PROCESSED_FILE, 'a') as fp:
+                fp.write('{}\n'.format(fn))
 
 
 
