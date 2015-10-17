72ab86db7f434c38b34f73700671e98c564da339
Krystof Litomisky <krystof@litomisky.com>
2015-10-17 12:21:32 -0700

    Sliding window: save regions' classification

--- python/sliding_window_training_data_generator.py
+++ python/sliding_window_training_data_generator.py
@@ -7,6 +7,8 @@ Written/tested with Python 2.7.6 and OpenCV 3.0.0
 '''
 
 import os
+import json
+import sys
 
 import cv2
 
@@ -35,6 +37,24 @@ class Training_data_generator(object):
         self.PROCESSED_FILE = 'processed.txt'
 
 
+    
+    def exit(self, posdict, negdict, neutral_dict):
+        '''
+        Save posdict to self.POSFILE, negdict to self.NEGFILE,
+             and neutral_dict to self.NEUTRAL_FILE
+        and exit.
+        '''
+        with open(self.POSFILE, 'w') as fp:
+            json.dump(posdict, fp, sort_keys=True, indent=2, separators=(',', ': '))
+        with open(self.NEGFILE, 'w') as fp:
+            json.dump(negdict, fp, sort_keys=True, indent=2, separators=(',', ': '))
+        with open(self.NEUTRAL_FILE, 'w') as fp:
+            json.dump(neutral_dict, fp, sort_keys=True, indent=2, separators=(',', ': '))
+
+        sys.exit()
+
+
+
     def train(self):
         '''
         TODO
@@ -47,6 +67,25 @@ class Training_data_generator(object):
                 pass
         with open(self.PROCESSED_FILE, 'r') as fp:
             processed = [s.strip() for s in fp.readlines()]
+
+
+        # open the current list of image regions
+        try:
+            with open(self.POSFILE, 'r') as fp:
+                posdict = json.loads(fp.read())
+        except:
+            posdict = {}
+        try:
+            with open(self.NEGFILE, 'r') as fp:
+                negdict = json.loads(fp.read())
+        except:
+            negdict = {}
+        try:
+            with open(self.NEUTRAL_FILE, 'r') as fp:
+                neutraldict = json.loads(fp.read())
+        except:
+            neutraldict = {}
+
 
         # for each file in the source directory
         for fn in os.listdir(self.SOURCEDIR):
@@ -90,28 +129,36 @@ class Training_data_generator(object):
                 # show the image
                 cv2.imshow('img', di)
 
+                # record this region in this dictionary (based on user input)
+                record_dict = None 
+
                 # get some valid input from the user
                 key = None
+                
                 while key not in [27, 65363, 65361, 65364]:
 
-                    key = cv2.waitKey(0)
+                    key = cv2.waitKey(0)                    
 
                     # escape: kill the program
                     if key == 27:
                         MSG('Exiting.')
+                        self.exit(posdict, negdict, neutraldict)
                         return
 
                     # right arrow: 65363 -> positive region
                     elif key == 65363:
                         MSG('POSITIVE')
+                        record_dict = posdict
 
                     # left  arrow: 65361 -> negative region
                     elif key == 65361:
                         MSG('negative')
+                        record_dict = negdict
 
                     # down  arrow: 65364 -> neutral / unknown / N/A / skip
                     elif key == 65364:
                         MSG('neutral')
+                        record_dict = neutraldict
 
                     # not valid input: ask again
                     else:
@@ -121,6 +168,18 @@ class Training_data_generator(object):
                             'down  arrow: not sure\n'
                             'escape     : exit the program')
 
+                # -- record this region ---------------------------------
+                regionstring = str(region) # minx, maxx, miny, maxy
+                #MSG('regionstring: {}'.format(regionstring))
+                
+                # does this image already have any regions in this dictionary?
+                if fn not in record_dict:
+                    record_dict[fn] = []
+                # make sure we don't record the same region multiple times
+                if regionstring not in record_dict[fn]:
+                    record_dict[fn].append(regionstring)
+                # -------------------------------------------------------
+
                 # get the next region
                 region = sw.get_next()
 
@@ -128,13 +187,14 @@ class Training_data_generator(object):
             with open(self.PROCESSED_FILE, 'a') as fp:
                 fp.write('{}\n'.format(fn))
 
+        # save the regions
+        self.exit(posdict, negdict, neutraldict)
+
 
 
 if __name__ == '__main__':
 
     # TODO: parse user input
-
-    # TODO: keep track of which images were already processed
     
     gen = Training_data_generator()
     gen.train()
